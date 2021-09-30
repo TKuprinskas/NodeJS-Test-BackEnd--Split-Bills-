6463441a9249f39324ad7ab191a5e34776f80c4d
Tomas Kuprinskas <t.kuprinskas@gmail.com>
2021-09-30 11:54:02 +0300

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,13 @@ const { jwtSecret } = require('./config');
 
 module.exports = {
   loggedIn: (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        const decodedToken = jwt.verify(token, jwtSecret);
-        req.userData = decodedToken;
-        next();
-    } else {
-        res.sendStatus(401);
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = jwt.verify(token, jwtSecret);
+      req.userData = decodedToken;
+      return next();
+    } catch (err) {
+      return res.status(401).send({ err: 'Validation failed' });
     }
   },
 };
